f805e6ea004bae7073f5ad1ca6ab991d06cd2d34
Maurício Junior <elyssadalva@gmail.com>
2021-09-13 16:13:27 -0300

    app finalizado

---
+++ lib/home.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController _controllerAlcool = TextEditingController();
+  TextEditingController _controllerGasolina = TextEditingController();
+  String _textoResultado = "Resultado";
+
+  void _calcular(){
+  
+    double? precoAlcool = double.tryParse(  _controllerAlcool.text);
+    double? precoGasolina = double.tryParse(  _controllerGasolina.text);
+
+     if(precoAlcool == null || precoGasolina == null){
+       setState(() {
+         _textoResultado = "Número invalido, digite numeros maiores que 0 e uitilize . para casas decimais ";
+       });
+       
+     }
+     else{
+       if(precoAlcool / precoGasolina >= 0.7){
+         setState(() {
+           _textoResultado = "Abasteça com gasolina"; 
+         });
+       }else{
+         _textoResultado = "Abasteça com álcool"; 
+         
+       }
+       
+
+     }
+   // _limparCampos();
+  }
+
+  void _limparCampos(){
+    _controllerGasolina.text = "";
+    _controllerAlcool.text = "";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Combustível"),
+        backgroundColor: Colors.orange.shade300,
+      ),
+      body: Container(
+        
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
+                child: Image.asset("images/logo.png"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text("Saiba qual a melhor opção de abastecimento para seu carro",
+                style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold),
+                
+                )
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Preço alcool"
+                ),
+                style: TextStyle(
+                  fontSize: 22
+                ),
+                controller: _controllerAlcool,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Preço gasolina"
+                ),
+                style: TextStyle(
+                  fontSize: 22
+                ),
+                controller: _controllerGasolina,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: RaisedButton(
+                  color: Colors. orange,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.all(15),
+                  child: Text("Calcular",
+                  style: TextStyle(
+                    fontSize: 20
+                   ),),
+                  onPressed: _calcular),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  _textoResultado,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold ),
+                    ),
+                )
+        ],),),
+      ),
+
+    );
+  }
+}
